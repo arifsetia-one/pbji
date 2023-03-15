e65b69686d124f16ad582430d558618fde9aa36e
candia <candia.hata20@students.amikom.ac.id>
2023-03-15 10:32:35 +0700

    mencoba grid

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,5 +1,26 @@
 <template>
-    wkwkwk profile
+    <!-- <div class="container px-6 max-w-md mx-auto sm:bg-red-500 md:bg-green-400 lg:bg-blue-400 xl:bg-pink-400">
+        <h3 class="text-4xl font-bold text-slate-700">Info pbji</h3>
+        <img src="../assets/hp.jpg" alt="">
+        <h2>mari belajar hp</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum rem delectus laborum facere explicabo itaque officia ad, obcaecati nemo! Consequatur magni nam repellat minus facilis voluptatem pariatur asperiores dolorum consequuntur excepturi debitis veritatis eum corporis, dignissimos provident itaque nesciunt architecto. Voluptate veniam repellendus temporibus accusamus veritatis, ipsum dolore tempora illum, minima sed facere. Magni iste culpa, repellat perferendis nihil consequuntur optio corporis blanditiis temporibus cumque? Fugit, sequi ullam? Cum perspiciatis, aspernatur tenetur illo labore similique et aliquam provident suscipit atque sapiente! Architecto quidem est unde. Ratione quam in iusto voluptatibus doloremque nemo architecto rem ullam deleniti? Consectetur rem libero officiis.
+        </p>
+        <div class="">
+            <a href="">Subscribe</a>
+        </div>
+    </div> -->
+
+    <div class="grid grid-cols-66">
+        <h1>hahaha</h1>
+        <h1>hahaha</h1>
+        <h1>hahaha</h1>
+        <h1>hahaha</h1>
+        <h1>hahaha</h1>
+        <h1>hahaha</h1>
+    </div>
+    <div class="gridgrid-cols-6">bb</div>
+
 </template>
 
 <script setup>

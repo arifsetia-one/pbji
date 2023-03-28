df9a3f9ca05c883548e8d0be288fcc5f827c8143
Arif Setiawan <arif.setiawan2209@students.amikom.ac.id>
2023-03-28 10:49:42 +0700

    done carousel

--- src/views/BeritaView.vue
+++ src/views/BeritaView.vue
@@ -1,4 +1,7 @@
 <template>
+    <head>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
+    </head>
     <!-- Carousel -->
     <div class="flex gap-4 mx-auto bg-slate-200 mt-10">
         <div id="controls-carousel" class="relative w-full" data-carousel="static">

67f9ab25f584f96fbaf227601465c6c2d2523bd7
Arif Setiawan <arif.setiawan2209@students.amikom.ac.id>
2023-03-29 09:21:25 +0700

    update feature/profile

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,16 +1,4 @@
 <template>
-    <!-- <div class="container px-6 max-w-md mx-auto sm:bg-red-500 md:bg-green-400 lg:bg-blue-400 xl:bg-pink-400">
-        <h3 class="text-4xl font-bold text-slate-700">Info pbji</h3>
-        <img src="../assets/hp.jpg" alt="">
-        <h2>mari belajar hp</h2>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum rem delectus laborum facere explicabo itaque officia ad, obcaecati nemo! Consequatur magni nam repellat minus facilis voluptatem pariatur asperiores dolorum consequuntur excepturi debitis veritatis eum corporis, dignissimos provident itaque nesciunt architecto. Voluptate veniam repellendus temporibus accusamus veritatis, ipsum dolore tempora illum, minima sed facere. Magni iste culpa, repellat perferendis nihil consequuntur optio corporis blanditiis temporibus cumque? Fugit, sequi ullam? Cum perspiciatis, aspernatur tenetur illo labore similique et aliquam provident suscipit atque sapiente! Architecto quidem est unde. Ratione quam in iusto voluptatibus doloremque nemo architecto rem ullam deleniti? Consectetur rem libero officiis.
-        </p>
-        <div class="">
-            <a href="">Subscribe</a>
-        </div>
-    </div> -->
-
     <div id="header" class="grid grid-cols-2">
         <div class="grid grid-cols-2">
             <div class="kotak"></div>
@@ -27,20 +15,6 @@
         <div class="foto">
             <img src="../assets/fotojjs-resize.jpg" alt="">
         </div>
-        <!-- <div class="kotak"></div>
-        <div class="judul">
-            <p>
-                Profile PBJI
-            </p>
-            <p>
-                Daerah Istimewa Yogyakarta
-            </p>
-            <h5>Pengurus Besar Jiu-Jitsu Indonesia</h5>
-        </div>
-        <div class=""></div>
-        <div class="foto">
-
-        </div> -->
     </div>
 
     <div id="about" class="">
@@ -49,7 +23,9 @@
         </div>
         <div class="fill">
             <p>
-                Pengurus Besar Ju-Jitsu Indonesia (PBJI) Daerah Istimewa Yogyakarta resmi dilantik pada tanggal 10 Februari 2022. PBJI Daerah Istimewa Yogyakarta dipimpin oleh Marjono, S.H. dan M. Herbowo E. yang akan bekerja dalam masa bakti 2022 - 2026.
+                Pengurus Besar Ju-Jitsu Indonesia (PBJI) Daerah Istimewa Yogyakarta resmi dilantik pada tanggal 10 Februari
+                2022. PBJI Daerah Istimewa Yogyakarta dipimpin oleh Marjono, S.H. dan M. Herbowo E. yang akan bekerja dalam
+                masa bakti 2022 - 2026.
             </p>
         </div>
     </div>
@@ -61,7 +37,9 @@
         <div class="clear"></div>
         <div class="fill2">
             <p>
-                Pelantikan dilakukan oleh Sekretaris Jenderal PBJI Pusat mewakili Ketua Umum PBJI Pusat, Dr. Dedy Triharyanto, di Gedung Garuda RSPAU dr. Suhardi Hardjolukito Jl. Janti Yogyakarta, Lanud Adisucipto, Jl. Ringroad Timur, Karang Jambe, Banguntapan, Bantul, D.I. Yogyakarta.
+                Pelantikan dilakukan oleh Sekretaris Jenderal PBJI Pusat mewakili Ketua Umum PBJI Pusat, Dr. Dedy
+                Triharyanto, di Gedung Garuda RSPAU dr. Suhardi Hardjolukito Jl. Janti Yogyakarta, Lanud Adisucipto, Jl.
+                Ringroad Timur, Karang Jambe, Banguntapan, Bantul, D.I. Yogyakarta.
             </p>
         </div>
     </div>
@@ -71,7 +49,9 @@
         <div class="visi">
             <h3>Visi</h3>
             <p>
-                Salah satu visi PBJI D.I. Yogyakarta adalah mencetak atlet dalam bentuk pembinaan atlet agar berprestasi dan mencari bibit - bibit atlet. Selain itu diharapkan bahwa Ju-Jitsu tidak hanya menjadi olahraga beladiri namun juga olahraga prestasi.
+                Salah satu visi PBJI D.I. Yogyakarta adalah mencetak atlet dalam bentuk pembinaan atlet agar berprestasi dan
+                mencari bibit - bibit atlet. Selain itu diharapkan bahwa Ju-Jitsu tidak hanya menjadi olahraga beladiri
+                namun juga olahraga prestasi.
             </p>
         </div>
         <div class="">bb</div>
@@ -96,36 +76,43 @@
     background-color: #636262;
     border-radius: 0px 20px 20px 0px;
 }
+
 .judul {
     margin: auto 0px;
 }
+
 .judul p {
     text-align: start;
     font-size: 40px;
     font-weight: bold;
 }
+
 .judul h5 {
     margin-top: 20px;
     font-style: italic;
     text-align: start;
     font-weight: bold;
 }
+
 .foto {
     background-color: rgb(255, 255, 255);
     display: flex;
     justify-content: flex-end;
     align-items: center;
 }
+
 .foto img {
     width: 416px;
     height: 314px;
     border-radius: 24px 0px 0px 24px;
 }
+
 #about {
     width: 800px;
     margin: 100px auto 20px;
     background-color: rgb(255, 255, 255);
 }
+
 #about2 {
     width: 800px;
     margin: 70px auto 130px;
@@ -138,6 +125,7 @@
     background-color: rgb(255, 255, 255);
     border-bottom: 10px solid #CE231C;
 }
+
 #about .judul1 h3 {
     font-size: 22px;
     font-weight: bolder;
@@ -148,9 +136,11 @@
     margin: 20px 0px 0px 0px;
     background-color: rgb(255, 255, 255);
 }
+
 .fill p {
     text-align: start;
 }
+
 .judul2 {
     background-color: rgb(255, 255, 255);
     width: 180px;
@@ -158,35 +148,43 @@
     border-bottom: 10px solid #CE231C;
     float: right;
 }
+
 .fill2 {
     width: fit-content;
-    margin: 20px 0px 0px 0px ;
+    margin: 20px 0px 0px 0px;
 }
+
 .fill2 p {
     text-align: end;
 }
+
 .clear {
     clear: right;
 }
+
 .visimisi {
     background-color: #CE231C;
     width: 100%;
 }
+
 .visimisi .visi {
     width: 800px;
     /* background-color: aquamarine; */
-    margin: 0px auto 0px ;
+    margin: 0px auto 0px;
     padding: 50px 0px 50px 0px;
 }
+
 .visi h3 {
     text-align: start;
     color: white;
     font-size: 32px;
 }
+
 .visi p {
     text-align: start;
     color: white;
 }
+
 /* .kotak {
     width: 122px;
     height: 400px;

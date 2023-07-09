5f74e92d9f21df095f319b63b7b16e8c1bb2f8ae
ArifSetiawan <arif.setiawan2209@students.amikom.c.id>
2023-07-10 01:07:11 +0700

    dummy commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import PresenceView from '../views/PresenceView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/profile',
-    name: 'profile',
-    component: () => import('../views/ProfileView.vue')
-  },
-  {
-    path: '/info',
-    name: 'info',
-    component: () => import('../views/InfoView.vue')
-  },
-  {
-    path: '/berita',
-    name: 'berita',
-    component: () => import('../views/BeritaView.vue')
-  },
-  {
-    path: '/event',
-    name: 'event',
-    component: () => import('../views/EventView.vue')
+    path: '/presence',
+    name: 'presence',
+    component: PresenceView
   },
 ]
 
